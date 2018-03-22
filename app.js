d2cdc65414254c1df9730e3541bee622b38f33fb
Adrian Berger <adigladi@n134-p106.eduroam.kth.se>
2018-03-22 14:47:51 +0100

    Cleanup

--- app.js
+++ app.js
@@ -19,11 +19,6 @@ var options = {
   function success(pos) {
     var crd = pos.coords;
     myLocation = {lat: crd.latitude, lng:crd.longitude}
-  
-    console.log('Your current position is:');
-    console.log(`Latitude : ${crd.latitude}`);
-    console.log(`Longitude: ${crd.longitude}`);
-    console.log(`More or less ${crd.accuracy} meters.`);
   }
   
   function error(err) {
